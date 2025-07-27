18f3d6a7d90a0a96c00282f103aba98ca3d9cef6
Devjangam48 <djangam4805@gmail.comgit config --global user.name DevJangam48>
2025-07-27 22:26:57 +0530

    feat: Prepare project for deployment

--- backend/server.js
+++ backend/server.js
@@ -13,8 +13,22 @@ connectDB();
 
 const app = express();
 app.use(express.json());
-console.log("JSON middleware executed");
-app.use(cors());
+
+const allowedOrigins = ["http://localhost:5173"];
+
+app.use(
+  cors({
+    origin: function (origin, callback) {
+      if (!origin) return callback(null, true);
+      if (allowedOrigins.indexOf(origin) === -1) {
+        const msg =
+          "The CORS policy for this site does not allow access from the specified Origin.";
+        return callback(new Error(msg), false);
+      }
+      return callback(null, true);
+    },
+  })
+);
 
 app.use("/api/auth", authRoutes);
 app.use("/api/products", productRoutes);
